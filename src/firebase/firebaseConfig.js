0c546f0fe6165e75c739e2eefb13b4e65d43b2f8
artemradz <artemradzivon@gmail.com>
2019-12-13 13:35:40 +0200

    add firebaseConfig

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCQ6Be7U80NoQrwCUvwi_KQ4OKBkBX01jw',
+  authDomain: 'animallookusers.firebaseapp.com',
+  databaseURL: 'https://animallookusers.firebaseio.com',
+  projectId: 'animallookusers',
+  storageBucket: 'animallookusers.appspot.com',
+  messagingSenderId: '677561625435',
+  appId: '1:677561625435:web:c0950e3a53eeaa58'
+};
